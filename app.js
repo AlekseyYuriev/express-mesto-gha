c735900576057ac69dfe2976e34d40ca37c87010
Aleksey Yuriev <alex11-90@yandex.by>
2023-11-19 00:32:08 +0300

    hotfix: wrongWayError added

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(json);
 app.use(userRouter);
 app.use(cardRouter);
 
+app.use((req, res, next) => {
+  res.status(404).json({
+      message: '404'
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
 })
